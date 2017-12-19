4a994daac6b1e25cb0261bc59c7c5fdbc581d3e5
Kristian Davidsen <krdav@cbs.dtu.dk>
2017-12-18 16:34:27 -0800

    repo start

---
+++ AMP.R
@@ -0,0 +1,17 @@
+
+library(devtools)
+devtools::load_all('sumrep')
+
+seq <- 'ATGGGGGG'
+partis_path <- 'ggg'
+ann <- annotateSequencesFromStrings(seq, partis_path=partis_path)
+
+### Make neutral simulation
+
+
+### ANARCI AHo annotate
+
+
+### Make Amrits data list
+
+
